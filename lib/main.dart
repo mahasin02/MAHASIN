d38c03bcf1323d1a6dfb3cab39c1b480474af9a7
mahasin02 <108251188+mahasin02@users.noreply.github.com>
2023-07-04 19:32:46 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,45 @@
-class PrimeNumbersScreen extends StatelessWidget {
-  final List<int> primeNumbers = [2];
+import 'dart:math';
+import 'package:flutter/material.dart';
 
-  PrimeNumbersScreen() {
-    for (int i = 3; i <= 45; i++) {
-      bool isPrime = true;
-      for (int j = 2; j <= i / 2; j++) {
-        if (i % j == 0) {
-          isPrime = false;
-          break;
-        }
-      }
-      if (isPrime) {
-        primeNumbers.add(i);
-      }
-    }
-  }
+bool isPrime(int number) {
+  if (number < 2) {
+    return false;
+  }
+  
+  for (int i = 2; i <= sqrt(number); i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Muhamad Mahasin Bagus Saputro (GENAP)',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Muhamad Mahasin Bagus Saputro (GENAP)')),
+        body: ListView.builder(
+          itemCount: 46, // Jumlah bilangan dari 0 hingga 45
+          itemBuilder: (context, index) {
+            final number = index;
+            final isNumberPrime = isPrime(number);
+            final result = isNumberPrime ? 'adalah bilangan prima' : 'bukan bilangan prima';
+            return ListTile(
+              title: Text('$number'),
+              subtitle: Text(result),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
